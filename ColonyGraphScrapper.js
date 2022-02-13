4dedf678f5ce4276223fa6d538d9aec1a77f67eb
nathanabram <nathan@nathanabram.com>
2022-02-13 12:33:37 -0700

    impliments start from cursor

--- ColonyGraphScrapper.js
+++ ColonyGraphScrapper.js
@@ -19,15 +19,18 @@ const dataTypes = [
 ];
 
 // function to fetch all of a given datatype
-const fetchAll = async (dataType, step = 1000) => {
+const fetchAll = async (dataType, cursors = {}, step = 1000) => {
+	let cursor = 0;
+	if (cursors) {
+		cursor = cursors[dataType] || 0;
+	}
 	let data = [];
 	let moreToFetch = true;
-	let skip = 0;
 	while (moreToFetch) {
 		let fetched = [];
 		// while data is still being returned
 		try {
-			fetched = await requestData(dataType, skip);
+			fetched = await requestData(dataType, cursor);
 			fetched = fetched[dataType];
 		} catch (err) {
 			console.log(err);
@@ -38,14 +41,23 @@ const fetchAll = async (dataType, step = 1000) => {
 		} else {
 			data = data.concat(fetched); // merge the arrays
 		}
-		skip += step; // increase so that the query fetches the next chunk of data
+		cursor += step; // increase so that the query fetches the next chunk of data
 	}
+	let newCursors = cursors;
+	newCursors[dataType] = cursor - step; // go back to the last complete fetch, since fetch from cursor didn't have data yet
+	fs.writeFileSync("./data/cursors.json", JSON.stringify(newCursors));
 	fs.writeFileSync(`./data/${dataType}`, JSON.stringify(data));
 	return data;
 };
 
-// for each datatype, execute fetchAll
+let cursors = {};
+try {
+	cursors = JSON.parse(fs.readFileSync("./data/cursors.json"));
+} catch {
+	cursors = {};
+}
 
+// for each datatype, execute fetchAll
 dataTypes.forEach((dataType) => {
-	fetchAll(dataType);
+	fetchAll(dataType, cursors);
 });
